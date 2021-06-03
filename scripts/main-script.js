49ac96113f281547a8bb2fa35f7b3e10fb226b6b
David Castrillón <david.castri@hotmail.es>
2021-06-03 18:04:23 +0200

    03/06/2021 - Hago los primeros 4 ejercicios del nivel 1.

---
+++ scripts/main-script.js
@@ -0,0 +1,20 @@
+/* 1- L'exercici consisteix a crear un fitxer anomenat ciber.html i mostrar per consola un 'Hola mundo' (per activar la consola ->; F12). Pista: Console.log() */
+console.log("Hola mundo!");
+
+/* 2- Crea un alert que mostri el teu nom: */
+alert("Me llamo David!");
+
+/* 3- Crea una variable que contingui el teu nom i un altre que contingui el teu cognom. */
+let nombre = "David";
+let apellido = "Castrillón";
+
+console.log(nombre + " " + apellido);
+
+/* 4- Crea dos variables amb dos números i fes una suma entre ells. */
+let numero1 = 5;
+let numero2 = 10;
+
+console.log("La suma entre " + numero1 + " y " + numero2 + " es " + (numero1 + numero2) + ".");
+
+/* 5- Crea una variable nota_examen juntament amb un alert que ens digui si l’examen ha estat aprovat o no juntament amb la nota. (caldrà utilitzar un condicional IF) */
+
